ded2d1fe9f1b1a0959963cf7fdd303d72946c1d9
John Otander <johnotander@gmail.com>
2016-01-11 18:08:06 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = function acos (options) {
+  options = options || {}
+
+  return true
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+import test from 'ava'
+import acos from './'
+
+test('acos does something awesome', t => {
+  t.plan(1)
+
+  t.true(acos())
+})
